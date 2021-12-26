8aff377722cc0cdedeb00776721c81ccd111407f
hugosilvam3 <96689648+hugosilvam3@users.noreply.github.com>
2021-12-26 08:57:17 -0500

    Add files via upload

---
+++ ESTRUECTURA DE DATOS - HUGO SILVA MEZA/ejercicio 25.py
@@ -0,0 +1,8 @@
+#HUGO STEVEN SILVA MEZA
+
+key = "hugo"
+password = input("Introduce la contraseña: ")
+if key == password.lower():
+    print("La contaseña coincide")
+else:
+    print("La contraseña no coincide")
